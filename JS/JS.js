f794c5eeeb7470076f7d4b35cd0feb8988fd4890
OurSummer2018 <43934702+OurSummer2018@users.noreply.github.com>
2018-10-07 21:46:32 +0300

    Add files via upload

---
+++ JS/JS.js
@@ -0,0 +1,20 @@
+var btn_prev = document.querySelector('#gallery .buttons .prev');
+var btn_next = document.querySelector('#gallery .buttons .next');
+var images = document.querySelectorAll('#gallery .photos img');
+
+var i = 0;
+
+btn_next.tick = function() {
+    images[i].classList.toggle('showed');
+    i++;
+
+    if (i >= images.length) {
+        i = 0;
+    }
+
+    images[i].classList.toggle('showed');
+}
+
+setInterval (function() {
+    btn_next.tick();
+}, 3000);
